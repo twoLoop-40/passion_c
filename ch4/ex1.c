3cf1739dcf8e2510accd6496400b247ca8ce0e2d
Lee Joon Ho <jh.lee@spira-t.com>
2021-03-03 01:12:07 +0900

    bit operation exercises

---
+++ ch4/ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int num1 = 5;
+    int num2;
+
+    num2 = num1 ^(-1);
+    printf("%d \n", num2);
+
+    return 0;
+}
---
+++ ch4/ex2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+    int num1 = 3;
+    int num2 = num1 << 3 >>2;
+
+    printf("%dX8/4=%d \n", num1, num2);
+
+}
